0ee87e988ae2ed2bc4c2f2f7f6e2f8b07348aabd
Gaurav-1997 <gauravish151297@gmail.com>
2021-09-30 13:28:08 +0530

    changes in playerService

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -9,17 +9,28 @@ import { Player } from '../models/player';
 })
 export class PlayerService {
 
-  apiBaseUPl ="https://localhost:44312/api/PlayerTables"
+  apiBaseUPl ="https://localhost:44312/api"
+  postId:number;
+  errorMessage: string;
+
   constructor(private http:HttpClient) { }
 
   getPlyers():Observable<Player[]>
   {
     console.log("Player Service called");
-    return this.http.get<Player[]>(this.apiBaseUPl);
+    return this.http.get<Player[]>(this.apiBaseUPl+"/PlayerTables");
   }
 
   addPlayer(player:Player)
   {
-    this.http.post(this.apiBaseUPl,player);
+    this.http.post<Player>(this.apiBaseUPl+"/PlayerTables",player).subscribe({
+      next: data => {
+        this.postId = data.id;
+    },
+    error: error => {
+        this.errorMessage = error.message;
+        console.error('There was an error!', error);
+    }
+    });
   }
 }
